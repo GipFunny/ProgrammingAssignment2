612d0e7bd86d087412ce92a86a0a02bd7b270e6b
Dan <daniel@smartpods.ca>
2018-09-17 10:38:15 -0300

    Second assignment completed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Second assignment: Lexical scoping
+## For increased efficiency, these functions are 
+## used to extract an inverse matrix from cache 
+## rather than calculation.
 
-## Write a short comment describing this function
+##--------------------------------------------------
+## Use:
+## 1. myCacheMatrix <- makeCacheMatrix()
+## 2. myCacheMatrix$set(matrix(rnorm(100), 10, 10))
+## 3. mySolvedMatrix <- cacheSolve(myCacheMatrix)
+
+## A second call to line 3 will produce the message: from cache
+
+## To view the data in the original matrix:
+## myCacheMatrix$get()
+
+## Create a special matrix that can be cached
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
 
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    
+    setsolved <- function (solved) m <<- solved
+    
+    getsolved <- function() m
+    
+    list(set = set, get = get, setsolved=setsolved, getsolved=getsolved)
 }
 
 
-## Write a short comment describing this function
+## Returns the solved matrix from cache, if available,
+## or calculation, if not.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolved()
+    
+    if (!is.null(m)){
+        message("from cache")
+        return (m)
+    }
+    
+    data <- x$get()
+    
+    m <- solve(data)
+    
+    x$setsolved(m)
+    
+    m
 }
